db8e4d1220042c86dc9a7ee7984a1527205acc8b
agustindamian258-hue <agustindamian258@gmail.com>
2026-06-27 11:24:00 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// Configuración de Firebase para LabuIA
+import { initializeApp } = "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqwJAdUyp_oxd4d8qTym-iFScSRzyDKeA",
+  authDomain: "labuia.firebaseapp.com",
+  projectId: "labuia",
+  storageBucket: "labuia.firebasestorage.app",
+  messagingSenderId: "386518529061",
+  appId: "1:386518529061:web:08c98a6d14eac3d28a7bcc"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, signInWithPopup, signOut, onAuthStateChanged, doc, setDoc, getDoc };
